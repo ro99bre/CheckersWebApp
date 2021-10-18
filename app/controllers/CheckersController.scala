f0adad915e506883c6e1aa0593f2861924a0ff91
Andreas Ly <andreas.ly96@gmail.com>
2021-10-18 14:13:47 +0200

    Merged Commits: Add move, undo, redo save & load methods to play controller by Andi Added Routes and corresponding functions for move, undo and redo by Robert

--- app/controllers/CheckersController.scala
+++ app/controllers/CheckersController.scala
@@ -13,6 +13,11 @@ import de.htwg.se.checkers.control.ControllerComponent.ControllerTrait
  *   /              Prints out Welcome Screen/Index
  *   /newGame       Starts a new Game
  *   /printGame     Prints the current Game Board
+ *   /move          Moves a Piece
+ *   /undo          Retrieves the last move
+ *   /redo          Redoes the retrived last move
+ *   /save          Saves Game
+ *   /load          Loads old Game
 */
 
 @Singleton
@@ -33,6 +38,31 @@ class CheckersController @Inject()(val controllerComponents: ControllerComponent
     }
 
     def printGame() = Action { implicit request: Request[AnyContent] =>
+        Ok(controller.gameToString)
+    }
+
+    def move(sx: Int, sy: Int, dx: Int, dy: Int) = Action { implicit request: Request[AnyContent] =>
+        controller.move(sx, sy, dx, dy)
+        Ok(controller.gameToString)
+    }
+
+    def undo() = Action { implicit request: Request[AnyContent] =>
+        controller.undo()
+        Ok(controller.gameToString)
+    }
+
+    def redo() = Action { implicit request: Request[AnyContent] =>
+        controller.redo()
+        Ok(controller.gameToString)
+    }
+
+    def save() = Action { implicit request: Request[AnyContent] =>
+        controller.save()
+        Ok(controller.gameToString)
+    }
+
+    def load() = Action { implicit request: Request[AnyContent] =>
+        controller.load()
         Ok(controller.gameToString)
     }
 }
