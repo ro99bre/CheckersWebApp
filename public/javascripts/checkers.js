3e148ce9690398e740263778ffb08c4e86f0b64f
Robert <robert.breuer@htwg-konstanz.de>
2021-11-22 16:10:04 +0100

    Refactored moving of Pieces to be done via Ajax calls

--- public/javascripts/checkers.js
+++ public/javascripts/checkers.js
@@ -1,16 +1,29 @@
+//Source tiles of move operation
 let sx = -1;
 let sy = -1;
 
 function move(y, x) {
-    //check if source tile has been selected
+    //Check if source tile has been selected
     if (sx === -1 && sy === -1) {
         sx = x;
         sy = y;
         document.getElementById("turn label").innerHTML = "Select Destination Tile";
+        //$("#turn label").html("Select Destination Tile")
+
+    //Complete move operation
     } else {
-        window.location.href = "http://localhost:9000/move/sx="+sx+"/sy="+sy+"/dx="+x+"/dy="+y;
-        sx = -1;
-        sy = -1;
+        $.ajax({
+            method: "GET",
+            url: "/move/sx="+sx+"/sy="+sy+"/dx="+x+"/dy="+y,
+            dataType: "json",
+
+            success: function (result) {
+                updateGameBoard(result);
+                sx = -1;
+                sy = -1;
+                document.getElementById("turn label").innerHTML = "Select Source Tile";
+            }
+        });
     }
 }
 
@@ -29,3 +42,92 @@ function save(fileName) {
 function load(fileName) {
     window.location.href = "http://localhost:9000/load?fileName=" + fileName;
 }
+
+class GameBoard {
+    constructor() {
+        this.game = [];
+        for(var i = 0; i < 8; i++) {
+            this.game[i] = [];
+            for(var j = 0; j < 8; j++) {
+                this.game[i][j] = Array(3);
+            }
+        }
+    }
+
+    fill(json) {
+        let cells = json.game.board.cells;
+        for(let jsonEntry=0; jsonEntry < cells.length; jsonEntry++) {
+            //Set Tile Color
+            this.game[cells[jsonEntry].y][cells[jsonEntry].x][0] = cells[jsonEntry].color;
+
+            //Check if Piece is located on Tile and it is not kicked
+            if(cells[jsonEntry].piece.piececolor !== "None" && cells[jsonEntry].piece.kicked !== "isKicked") {
+                this.game[cells[jsonEntry].y][cells[jsonEntry].x][1] = cells[jsonEntry].piece.piececolor;
+
+                //Check if Piece is a Queen
+                if(cells[jsonEntry].piece.queen === "isQueen") {
+                    this.game[cells[jsonEntry].y][json[jsonEntry].x][2] = true;
+                }
+            }
+        }
+    }
+}
+
+let gameBoard = new GameBoard()
+
+function printGameBoard(){
+    for(let row=0; row < 8; row++){
+        for(let col=0; col < 8; col++){
+            //Non Empty Cell
+            if(gameBoard.game[row][col][1] !== undefined) {
+                //Queen Piece
+                if(gameBoard.game[row][col][2] === true) {
+                    $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + gameBoard.game[row][col][1] + 'queen' + '.jpg');
+                    $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile' + gameBoard.game[row][col][1] + ' Queen');
+                //Normal Piece
+                } else {
+                    $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + gameBoard.game[row][col][1] +'.jpg');
+                    $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile' + gameBoard.game[row][col][1] + ' Piece');
+                }
+            //Empty Cell
+            } else {
+                $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + '.jpg');
+                $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile');
+            }
+        }
+    }
+}
+
+function updateGameBoard(jsonData) {
+    gameBoard = new GameBoard();
+    gameBoard.fill(jsonData);
+    printGameBoard();
+}
+
+function createClickListener(){
+    for(let row=0; row < 8; row++){
+        for(let col=0; col < 8; col++){
+            $("#field-" + row + "-" + col).click(function() {
+                move(row, col);
+            })
+        }
+    }
+}
+
+function initGame() {
+    $.ajax({
+        method: "GET",
+        url: "/jsonGame",
+        dataType: "json",
+
+        success: function (jsonData) {
+            gameBoard.fill(jsonData);
+            printGameBoard();
+            createClickListener();
+        }
+    });
+}
+
+$( document ).ready(function() {
+    initGame();
+});
