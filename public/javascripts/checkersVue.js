d69e0150ad12d4eaf2e8920855966060353fe3a3
Robert <robert.breuer@htwg-konstanz.de>
2021-12-11 11:17:53 +0100

    Improved GameBoard implementation

--- public/javascripts/checkersVue.js
+++ public/javascripts/checkersVue.js
@@ -1,41 +1,8 @@
+//Banner Messages
 function showConnectionErrorBanner() {
     var errorBanner = $("<div class=\"alert alert-danger\" role=\"alert\"></div>")
         .text("Oops! Updating the Game failed! Please try to reload the page");
     $("nav").after(errorBanner);
-}
-
-//Source tiles of move operation
-let sx = -1;
-let sy = -1;
-
-function move(y, x) {
-    console.log(y.toString() + x.toString())
-    //Check if source tile has been selected
-    if (sx === -1 && sy === -1) {
-        sx = x;
-        sy = y;
-        document.getElementById("turn label").innerHTML = "Select Destination Tile";
-        //$("#turn label").html("Select Destination Tile")
-
-    //Complete move operation
-    } else {
-        $.ajax({
-            method: "GET",
-            url: "/move/sx="+sx+"/sy="+sy+"/dx="+x+"/dy="+y,
-            dataType: "json",
-
-            success: function (result) {
-                updateGameBoard(result);
-                //Vor erneutem Rendering warten, bis Daten vollständig
-                sx = -1;
-                sy = -1;
-                document.getElementById("turn label").innerHTML = "Select Source Tile";
-            },
-            error: function (){
-                showConnectionErrorBanner();
-            }
-        });
-    }
 }
 
 function showGameErrorBanner(message) {
@@ -44,7 +11,7 @@ function showGameErrorBanner(message) {
     $("nav").after(errorBanner);
 
     setTimeout(
-        function() {
+        function () {
             $(errorBanner).remove();
         }, 3000);
 }
@@ -55,202 +22,51 @@ function showGameSuccessBanner(message) {
     $("nav").after(successBanner);
 
     setTimeout(
-        function() {
+        function () {
             $(successBanner).remove();
         }, 3000);
 }
 
-function undo() {
-    $.ajax({
-        method: "GET",
-        url: "/undo",
-        dataType: "json",
-
-        success: function (result) {
-            if(result === "Failed"){
-                showGameErrorBanner("Failed to undo last step!")
-            } else {
-                updateGameBoard(result);
-            }
-        },
-        error: function (){
-            showConnectionErrorBanner();
-        }
-    });
-}
-
-function redo() {
-    $.ajax({
-        method: "GET",
-        url: "/redo",
-        dataType: "json",
-
-        success: function (result) {
-            if(result === "Failed"){
-                showGameErrorBanner("Failed to redo last step!")
-            } else {
-                updateGameBoard(result);
-            }
-        },
-        error: function (){
-            showConnectionErrorBanner();
-        }
-    });
-}
-
-function save(fileName) {
-    $.ajax({
-        method: "GET",
-        url: "/save?fileName=" + fileName,
-        dataType: "json",
-
-        success: function (result) {
-            if(result !== "Save Failed"){
-                updateGameBoard(result);
-                showGameSuccessBanner("Successfully saved Game " + fileName);
-            } else {
-                showGameErrorBanner("Failed to save game!")
-            }
-        },
-        error: function (){
-            showConnectionErrorBanner();
-        }
-    });
-}
-
-function load(fileName) {
-    $.ajax({
-        method: "GET",
-        url: "/load?fileName=" + fileName,
-        dataType: "json",
-
-        success: function (result) {
-            if(result !== "Load Failed"){
-                updateGameBoard(result);
-                showGameSuccessBanner("Successfully loaded Game " + fileName);
-            } else {
-                showGameErrorBanner("Failed to load game!")
-            }
-        },
-        error: function (){
-            showConnectionErrorBanner();
-        }
-    });
-}
 
 class GameBoard {
     constructor(jsonData) {
+        //console.log(jsonData);
+
+        //Create Datastructure
         this.game = [];
-        for(var i = 0; i < 8; i++) {
+        for (var i = 0; i < 8; i++) {
             this.game[i] = [];
-            for(var j = 0; j < 8; j++) {
+            for (var j = 0; j < 8; j++) {
                 this.game[i][j] = Array(3);
             }
         }
-        console.log("newGameBoard")
-        this.fill(jsonData)
 
-    }
-
-    fill(json) {
-        console.log(json)
-        let cells = json.game.board.cells;
-        for(let jsonEntry=0; jsonEntry < cells.length; jsonEntry++) {
+        //Fill Datastructure
+        let cells = jsonData.game.board.cells;
+        for (let jsonEntry = 0; jsonEntry < cells.length; jsonEntry++) {
             //Set Tile Color
             this.game[cells[jsonEntry].y][cells[jsonEntry].x][0] = cells[jsonEntry].color;
-            console.log(this.game[cells[jsonEntry].y][cells[jsonEntry].x][0])
 
             //Check if Piece is located on Tile and it is not kicked
-            if(cells[jsonEntry].piece.piececolor !== "None" && cells[jsonEntry].piece.kicked !== "isKicked") {
+            if (cells[jsonEntry].piece.piececolor !== "None" && cells[jsonEntry].piece.kicked !== "isKicked") {
                 this.game[cells[jsonEntry].y][cells[jsonEntry].x][1] = cells[jsonEntry].piece.piececolor;
 
                 //Check if Piece is a Queen
-                if(cells[jsonEntry].piece.queen === "isQueen") {
+                if (cells[jsonEntry].piece.queen === "isQueen") {
                     this.game[cells[jsonEntry].y][cells[jsonEntry].x][2] = true;
                 }
             }
         }
-        console.log("Finished filling")
-        console.log(this.game)
+        //console.log(this.game)
     }
 }
 
-//let gameBoard = new GameBoard()
-
-//function printGameBoard(){
-//    for(let row=0; row < 8; row++){
-//        for(let col=0; col < 8; col++){
-//            //Non Empty Cell
-//            if(gameBoard.game[row][col][1] !== undefined) {
-//                //Queen Piece
-//                if(gameBoard.game[row][col][2] === true) {
-//                    $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + gameBoard.game[row][col][1] + 'queen' + '.jpg');
-//                    $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile' + gameBoard.game[row][col][1] + ' Queen');
-//                //Normal Piece
-//                } else {
-//                    $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + gameBoard.game[row][col][1] +'.jpg');
-//                    $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile' + gameBoard.game[row][col][1] + ' Piece');
-//                }
-//            //Empty Cell
-//            } else {
-//                $("#field-" + row + "-" + col).attr('src', 'assets/images/game/' + gameBoard.game[row][col][0] + '.jpg');
-//                $("#field-" + row + "-" + col).attr('alt', gameBoard.game[row][col][0] + ' Tile');
-//            }
-//        }
-//    }
-//}
-
-function updateGameBoard(jsonData) {
-    //gameBoard = new GameBoard(jsonData);
-    //gameBoard.fill(jsonData);
-}
-
-function createClickListener(){
-
-    $("#undo-button").click(function() {
-        undo();
-    })
-
-    $("#redo-button").click(function() {
-        redo();
-    })
-
-    $("#save-button").click(function() {
-        save(savename.value);
-        $("#saveModal").modal('toggle');
-    })
-
-    $("#load-button").click(function() {
-        load(loadname.value);
-        $("#loadModal").modal('toggle');
-    })
-}
-
-function initGame() {
-    $.ajax({
-        method: "GET",
-        url: "/jsonGame",
-        dataType: "json",
-
-        success: function (jsonData) {
-            console.log("success")
-            //gameBoard.fill(jsonData);
-            createVue(jsonData);
-            console.log("Created Vue")
-            console.log(gameBoard.game[1][1][0]);
-            createClickListener();
-        },
-        error: function (){
-            showConnectionErrorBanner();
-        }
-    });
-}
 
 function connectWebSocket() {
     var websocket = new WebSocket("ws://localhost:9000/websocket");
-    websocket.setTimeout
+    websocket.setTimeout;
 
-    websocket.onopen = function(event) {
+    websocket.onopen = function (event) {
         console.log("Connected to Websocket");
     }
 
@@ -265,92 +81,206 @@ function connectWebSocket() {
     websocket.onmessage = function (e) {
         if (typeof e.data === "string") {
             let json = JSON.parse(e.data);
-            updateGameBoard(json)
+            updateGameBoard(json);
         }
 
     };
 }
 
-$( document ).ready(function() {
+
+//Source tiles of move operation
+let srow = -1;
+let scol = -1;
+
+function move(row, col) {
+    //console.log(row.toString() + col.toString())
+
+    //Check if source tile has been selected
+    if (scol === -1 && srow === -1) {
+        scol = col;
+        srow = row;
+        document.getElementById("turn label").innerHTML = "Select Destination Tile";
+        //$("#turn label").html("Select Destination Tile")
+
+        //Complete move operation
+    } else {
+        $.ajax({
+            method: "GET",
+            url: "/move/sx=" + scol + "/sy=" + srow + "/dx=" + col + "/dy=" + row,
+            dataType: "json",
+
+            success: function (result) {
+                updateGameBoard(result);
+                scol = -1;
+                srow = -1;
+                document.getElementById("turn label").innerHTML = "Select Source Tile";
+            },
+            error: function () {
+                showConnectionErrorBanner();
+            }
+        });
+    }
+}
+
+function undo() {
+    $.ajax({
+        method: "GET",
+        url: "/undo",
+        dataType: "json",
+
+        success: function (result) {
+            if (result === "Failed") {
+                showGameErrorBanner("Failed to undo last step!")
+            } else {
+                updateGameBoard(result);
+            }
+        },
+        error: function () {
+            showConnectionErrorBanner();
+        }
+    });
+}
+
+function redo() {
+    $.ajax({
+        method: "GET",
+        url: "/redo",
+        dataType: "json",
+
+        success: function (result) {
+            if (result === "Failed") {
+                showGameErrorBanner("Failed to redo last step!")
+            } else {
+                updateGameBoard(result);
+            }
+        },
+        error: function () {
+            showConnectionErrorBanner();
+        }
+    });
+}
+
+function save(fileName) {
+    $.ajax({
+        method: "GET",
+        url: "/save?fileName=" + fileName,
+        dataType: "json",
+
+        success: function (result) {
+            if (result !== "Save Failed") {
+                updateGameBoard(result);
+                showGameSuccessBanner("Successfully saved Game " + fileName);
+            } else {
+                showGameErrorBanner("Failed to save game!")
+            }
+        },
+        error: function () {
+            showConnectionErrorBanner();
+        }
+    });
+}
+
+function load(fileName) {
+    $.ajax({
+        method: "GET",
+        url: "/load?fileName=" + fileName,
+        dataType: "json",
+
+        success: function (result) {
+            if (result !== "Load Failed") {
+                updateGameBoard(result);
+                showGameSuccessBanner("Successfully loaded Game " + fileName);
+            } else {
+                showGameErrorBanner("Failed to load game!")
+            }
+        },
+        error: function () {
+            showConnectionErrorBanner();
+        }
+    });
+}
+
+function createClickListener() {
+
+    $("#undo-button").click(function () {
+        undo();
+    })
+
+    $("#redo-button").click(function () {
+        redo();
+    })
+
+    $("#save-button").click(function () {
+        save(savename.value);
+        $("#saveModal").modal('toggle');
+    })
+
+    $("#load-button").click(function () {
+        load(loadname.value);
+        $("#loadModal").modal('toggle');
+    })
+}
+
+
+$(document).ready(function () {
+
     connectWebSocket();
-    console.log("connectWebSocket")
-    initGame();
-    console.log("InitGame")
-    //console.log(gameBoard.game[1][1][0])
-});
+    $.ajax({
+        method: "GET",
+        url: "/jsonGame",
+        dataType: "json",
+
+        success: function (jsonData) {
+            createVue(jsonData);
+            createClickListener();
+        },
+        error: function () {
+            showConnectionErrorBanner();
+        }
+    });
+})
+
+var vm;
 
 function createVue(jsonData) {
-    var vm = new Vue({
+    vm = new Vue({
         el: "#game-board",
-        //data: {
-        //    gameBoard: new GameBoard()
-        //},
-        data: function() {
+        data: function () {
             return {
                 gameBoard: new GameBoard(jsonData)
             }
         },
         methods: {
-            getTile (row, col) {
-                path = 'assets/images/game/'
-                //Non Empty Cell
-                if(this.gameBoard.game[row][col][1] !== undefined) {
-                    //Queen Piece
-                    if(this.gameBoard.game[row][col][2] === true) {
-                        variable = path + this.gameBoard.game[row][col][0] + this.gameBoard.game[row][col][1] + 'queen' + '.jpg'
-                        console.log("Queen:" + variable)
-                        return variable
-                    //Normal Piece
-                    } else {
-                        variable = path + this.gameBoard.game[row][col][0] + this.gameBoard.game[row][col][1] +'.jpg'
-                        console.log("Normal:" + variable)
-                        return variable
-                    }
-                //Empty Cell
-                } else {
-                    variable = path + this.gameBoard.game[row][col][0] + '.jpg'
-                    console.log("Empty:" + "Row:" + row + "Col:" + col + " " + variable)
-                    return variable
-                }
-            },
-
-            getPosition (x, y) {
-                return x.toString() + " " + y.toString()
+            getPosition(x, y) {
+                return x.toString() + " " + y.toString();
                 //return gameBoard.game[x][y][0]
             },
 
-            moveTile (col, row) {
-                console.log(row.toString() + col.toString())
-                //Check if source tile has been selected
-                if (sx === -1 && sy === -1) {
-                    sx = row;
-                    sy = col;
-                    document.getElementById("turn label").innerHTML = "Select Destination Tile";
-                    //$("#turn label").html("Select Destination Tile")
-            
-                //Complete move operation
+            getTile(row, col) {
+                path = 'assets/images/game/'
+                //Non Empty Cell
+                if (this.gameBoard.game[row][col][1] !== undefined) {
+                    //Queen Piece
+                    if (this.gameBoard.game[row][col][2] === true) {
+                        return path + this.gameBoard.game[row][col][0] + this.gameBoard.game[row][col][1] + 'queen' + '.jpg';
+                        //Normal Piece
+                    } else {
+                        return path + this.gameBoard.game[row][col][0] + this.gameBoard.game[row][col][1] + '.jpg';
+                    }
+                    //Empty Cell
                 } else {
-                    console.log(sx + " " + sy)
-                    $.ajax({
-                        method: "GET",
-                        url: "/move/sx="+sx+"/sy="+sy+"/dx="+row+"/dy="+col,
-                        dataType: "json",
-            
-                        success: function (result) {
-                            //updateGameBoard(result);
-                            vm.gameBoard = new GameBoard(result)
-                            //this.gameBoard.fill(result)
-                            sx = -1;
-                            sy = -1;
-                            document.getElementById("turn label").innerHTML = "Select Source Tile";
-                        },
-                        error: function (){
-                            showConnectionErrorBanner();
-                        }
-                    });
+                    return path + this.gameBoard.game[row][col][0] + '.jpg';
                 }
-            
+            },
+
+            moveTile(col, row) {
+                move(col, row);
             }
+
         }
     })
+}
+
+function updateGameBoard(jsonData) {
+    vm.gameBoard = new GameBoard(jsonData);
 }
